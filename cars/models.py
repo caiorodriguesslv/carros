2b8cea7dfeeba812b2152e4c3e148728d122fc16
Caio Rodrigues <caio.rodrigues@indt.org.br>
2024-11-29 10:27:15 -0300

    Model: photo and plate fields

--- cars/models.py
+++ cars/models.py
@@ -14,7 +14,9 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate =  models.CharField(max_length=10, blank=True, null=True)
     value = models.DecimalField(max_digits=8, decimal_places=2)
+    photo = models.ImageField(upload_to="cars/", blank=True, null=True)
 
     def __str__(self):
         return self.model
